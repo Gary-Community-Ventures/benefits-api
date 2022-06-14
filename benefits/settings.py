2c3c095e795c96b93c72da96b2ec415cdb0518f0
Brian Hiatt <brian.hiatt@gmail.com>
2022-06-14 13:32:28 -0600

    DEBUG defaulted to false unless overriden by env variable

--- benefits/settings.py
+++ benefits/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = config('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
