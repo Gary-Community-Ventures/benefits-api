ebb0ab794227baf4131ef6061e36f802445b491d
Brian Hiatt <brian.hiatt@gmail.com>
2022-09-06 11:23:48 -0600

    Added language translation support with Spanish files.

--- benefits/settings.py
+++ benefits/settings.py
@@ -43,6 +43,9 @@ FPL2021 = {
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -106,6 +109,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware'
 ]
 
 ROOT_URLCONF = 'benefits.urls'
