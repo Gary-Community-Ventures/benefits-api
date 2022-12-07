689d87dcc3c7bb184536bed3e73e084b35c03044
Brian Hiatt <brian.hiatt@gmail.com>
2022-12-07 14:56:10 -0700

    Added a custom UserManager class to allow for superuser creation on the command line.

--- authentication/models.py
+++ authentication/models.py
@@ -1,8 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
+
+class UserManager(BaseUserManager):
+    def create_user(self, email_or_cell, tcpa_consent, password=None):
+        """
+        Creates and saves a User with the given email or cell and password.
+        """
+        if not email_or_cell:
+            raise ValueError('Users must have an email address or cell phone number')
+
+        user = self.model(
+            email_or_cell=email_or_cell,
+            tcpa_consent=tcpa_consent
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email_or_cell, tcpa_consent, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email_or_cell=email_or_cell,
+            password=password,
+            tcpa_consent=tcpa_consent
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 # Users are created by the screener when someone signs up to provide feedback
 # or be notified of future benefits that are available to them. The unique id
@@ -19,7 +51,10 @@ class User(AbstractUser):
     send_offers = models.BooleanField(default=False)
     send_updates = models.BooleanField(default=False)
 
+    objects = UserManager()
+
     USERNAME_FIELD = 'email_or_cell'
+    REQUIRED_FIELDS = ['tcpa_consent']
 
     def save(self, **kwargs):
         self.cell = self.cell or None
