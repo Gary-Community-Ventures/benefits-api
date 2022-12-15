4874dbc023ebf9ddad164a02ff859f2d5a97fd76
Caleb <caleblpena@gmail.com>
2022-12-15 11:53:52 -0700

    Added a _condition method

--- programs/programs/andso/andso.py
+++ programs/programs/andso/andso.py
@@ -21,6 +21,27 @@ class Andso():
             "failed": []
         }
 
+    def calc_eligibility(self):
+
+        #No SSI
+        self._condition(not self.screen.has_ssi,
+                        "Must not be receiving SSI",
+                        "Does not receive SSI")
+
+        # No TANIF
+        self._condition(not self.screen.has_tanf,
+                        "Must not be eligible for TANF",
+                        "Is not eligible for TANF")
+
+        # Right age
+
+        # Has disability/blindness
+
+        # Meets income qualifications
+
+    def calc_value(self):
+        self.value = 0
+    
         self.calc_eligibility()
 
         self.calc_value()
@@ -32,21 +53,8 @@ class Andso():
     def _passed(self, msg):
         self.eligibility["passed"].append(msg)
 
-    def calc_eligibility(self):
-
-        #No SSI
-        if self.screen.has_ssi:
-            self._failed("Must not be receiving SSI")
+    def _condition(self, condition, failed_msg, pass_msg):
+        if condition is True:
+            self._passed(pass_msg)
         else:
-            self._passed("Does not receive SSI")
-
-        # No TANIF
-
-        # Right age
-
-        # Has disability/blindness
-
-        # Meets income qualifications
-
-    def calc_value(self):
-        self.value = 0
+            self._failed(failed_msg)
