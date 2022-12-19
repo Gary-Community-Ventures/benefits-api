874a386bb4e1f0cc3945cda5d2c09829d964308d
Caleb <caleblpena@gmail.com>
2022-12-19 10:58:46 -0700

    added marital status to AND-CS calculation

--- programs/programs/andcs/andcs.py
+++ programs/programs/andcs/andcs.py
@@ -98,10 +98,43 @@ class Andcs():
 
     def calc_value(self):
         self.value = 0
-        for member in self.posible_eligble_members:
-            member_value = max(0, Andcs.grant_standard - member["countable_income"])
-            self.value += member_value
+
+        # remove any possible couples
+        possible_couples = set()
+        for possible_eligible_member in self.posible_eligble_members:
+            member = possible_eligible_member['member']
+            countable_income = possible_eligible_member['countable_income']
+
+            if member.id not in possible_couples:
+                # Check if there is a couple, and only count SSI for one couple
+                # This means that AND-SO might be inacurate for couples
+
+                if member.relationship == 'headOfHousehold':
+                    for household_member in self.posible_eligble_members:
+                        if household_member['member'].relationship in ('spouse', 'domesticPartner'):
+                            # head of house married to this person
+                            possible_couples.add(household_member['member'].id)
+                            break
+                elif member.relationship in ('spouse', 'domesticPartner'):
+                    # married to head of house
+                    possible_couples.add(
+                        self.screen.household_members.filter(relationship='headOfHousehold')[0].id)
+                elif member.relationship in ('parent', 'fosterParent', 'stepParent', 'grandParent'):
+                    # might be married to someone with same relationship to head of house
+                    for person in self.posible_eligble_members:
+                        person = person['member']
+                        if person.relationship == member.relationship and person.id != member.id:
+                            # first other person with same relationship is excluded
+                            # only works for first couple
+                            possible_couples.add(person.id)
+                            break
+
+                # add to total AND-SO value
+                member_value = max(0, Andcs.grant_standard - countable_income)
+                self.value += member_value
+
         self.value *= 12
+
 
     def _failed(self, msg):
         self.eligibility["eligible"] = False
