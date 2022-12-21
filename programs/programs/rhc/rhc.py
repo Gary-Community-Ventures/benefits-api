f907402474aacf88b989238dfbe4630832038fd5
Caleb <caleblpena@gmail.com>
2022-12-21 14:22:27 -0700

    started adding Reproductive Health Care benifit

---
+++ programs/programs/rhc/rhc.py
@@ -0,0 +1,53 @@
+from programs.programs.tanf.tanf import calculate_tanf
+
+
+def calculate_rhc(screen, data):
+    rhc = Rhc(screen)
+    eligibility = rhc.eligibility
+    value = rhc.value
+
+    calculation = {
+        'eligibility': eligibility,
+        'value': value
+    }
+
+    return calculation
+
+
+class Rhc():
+    amount = 268
+
+    def __init__(self, screen):
+        self.screen = screen
+
+        self.eligibility = {
+            "eligible": True,
+            "passed": [],
+            "failed": []
+        }
+
+        self.calc_eligibility()
+
+        self.calc_value()
+
+    def calc_eligibility(self):
+        pass
+
+    def calc_value(self):
+        self.value = Rhc.amount
+
+    def _failed(self, msg):
+        self.eligibility["eligible"] = False
+        self.eligibility["failed"].append(msg)
+
+    def _passed(self, msg):
+        self.eligibility["passed"].append(msg)
+
+    def _condition(self, condition, msg):
+        if condition is True:
+            self._passed(msg)
+        else:
+            self._failed(msg)
+
+    def _between(self, value, min_val, max_val):
+        return min_val <= value <= max_val
