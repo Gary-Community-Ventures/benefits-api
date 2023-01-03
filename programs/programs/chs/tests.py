18b9ee8a02878fccb5f3c57034f9353bc5a93780
Caleb <caleblpena@gmail.com>
2023-01-03 13:48:26 -0700

    added tests for Colorado Head Start

---
+++ programs/programs/chs/tests.py
@@ -0,0 +1,82 @@
+from django.test import TestCase
+from programs.programs.chs.chs import Chs
+from screener.models import Screen, HouseholdMember, IncomeStream
+from django.conf import settings
+
+
+class TestChsPension(TestCase):
+    def setUp(self):
+        self.screen1 = Screen.objects.create(
+            agree_to_tos=True,
+            zipcode='80205',
+            county='Denver County',
+            household_size=2,
+            household_assets=0,
+        )
+        self.person1 = HouseholdMember.objects.create(
+            screen=self.screen1,
+            relationship='headOfHousehold',
+            age=30,
+            student=False,
+            student_full_time=False,
+            pregnant=False,
+            unemployed=False,
+            worked_in_last_18_mos=True,
+            visually_impaired=False,
+            disabled=False,
+            veteran=False,
+            has_income=False,
+            has_expenses=False,
+        )
+        self.person2 = HouseholdMember.objects.create(
+            screen=self.screen1,
+            relationship='child',
+            age=4,
+            student=False,
+            student_full_time=False,
+            pregnant=False,
+            unemployed=False,
+            worked_in_last_18_mos=True,
+            visually_impaired=False,
+            disabled=False,
+            veteran=False,
+            has_income=False,
+            has_expenses=False,
+        )
+
+    def test_chs_visualy_impaired_is_eligible(self):
+        chs = Chs(self.screen1)
+        eligibility = chs.eligibility
+
+        self.assertTrue(eligibility["eligible"])
+        self.assertIn(
+            f"Must have a child age {Chs.max_age} or less",
+            eligibility['passed'])
+        self.assertIn(
+            f"Income of $0 must be less than ${int(settings.FPL2022[2]/12)}",
+            eligibility['passed'])
+        self.assertEqual(len(eligibility['failed']), 0)
+
+    def test_chs_failed_all_conditions(self):
+        income = IncomeStream.objects.create(
+            screen=self.screen1,
+            household_member=self.person1,
+            type='wages',
+            amount=2000,
+            frequency='monthly'
+        )
+        self.screen1.save()
+        self.person2.age=6
+        self.person2.save()
+
+        chs = Chs(self.screen1)
+        eligibility = chs.eligibility
+
+        self.assertFalse(eligibility["eligible"])
+        self.assertIn(
+            f"Must have a child age {Chs.max_age} or less",
+            eligibility['failed'])
+        self.assertIn(
+            f"Income of $2000 must be less than ${int(settings.FPL2022[2]/12)}",
+            eligibility['failed'])
+        self.assertEqual(len(eligibility['passed']), 0)
