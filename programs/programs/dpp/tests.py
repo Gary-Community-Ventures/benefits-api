6c4c3b4ef14f652e3dc41a396c73774c75768050
Caleb <caleblpena@gmail.com>
2023-01-10 13:40:20 -0700

    removed specific test conditions

--- programs/programs/dpp/tests.py
+++ programs/programs/dpp/tests.py
@@ -42,19 +42,11 @@ class TestDpp(TestCase):
             has_expenses=False,
         )
 
-    def test_screen_exits(self):
-        self.assertEqual(self.screen1.agree_to_tos, True)
-        self.assertEqual(self.person1.screen, self.screen1)
-        self.assertEqual(self.person2.screen, self.screen1)
-
     def test_dpp_has_preschooler(self):
         dpp = Dpp(self.screen1)
         eligibility = dpp.eligibility
 
         self.assertTrue(eligibility["eligible"])
-        self.assertIn(f"Must have a child between the ages of {Dpp.min_age} and {Dpp.max_age}", eligibility['passed'])
-        self.assertIn(f"Must live in Denver", eligibility['passed'])
-        self.assertEqual(len(eligibility['failed']), 0)
     
     def test_dpp_doesnt_have_preschooler(self):
         self.person2.age = 5
@@ -63,5 +55,3 @@ class TestDpp(TestCase):
         eligibility = dpp.eligibility
 
         self.assertFalse(eligibility["eligible"])
-        self.assertIn(f"Must have a child between the ages of {Dpp.min_age} and {Dpp.max_age}", eligibility['failed'])
-        self.assertEqual(len(eligibility['passed']), 1)
