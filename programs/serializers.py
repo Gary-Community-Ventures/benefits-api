f28fef6037a1a149ee8cc4da17ca58fcd0312677
Caleb <caleblpena@gmail.com>
2023-01-31 16:04:17 -0700

    added UrgentNeedSerializer

--- programs/serializers.py
+++ programs/serializers.py
@@ -1,15 +1,23 @@
-from programs.models import Program, Navigator
+from programs.models import Program, UrgentNeed, Navigator
 from parler_rest.serializers import TranslatableModelSerializer, \
     TranslatedFieldsField
+
 
 class NavigatorSerializer(TranslatableModelSerializer):
     class Meta:
         model = Navigator
         fields = ('name', 'phone_number', 'email', 'assistance_link', 'description')
 
+
 class ProgramSerializer(TranslatableModelSerializer):
     translations = TranslatedFieldsField(shared_model=Program)
 
     class Meta:
         model = Program
+        fields = '__all__'
+
+
+class UrgentNeedSerializer(TranslatableModelSerializer):
+    class Meta:
+        model = UrgentNeed
         fields = '__all__'
