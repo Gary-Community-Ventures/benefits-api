bb050e2271e15297c5c59e1df714843d7cedf7a1
Caleb <caleblpena@gmail.com>
2023-01-31 15:24:28 -0700

    added UrgentNeed model to database

--- programs/admin.py
+++ programs/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-from .models import Program, Navigator
+from .models import Program, UrgentNeed, Navigator
 from parler.admin import TranslatableAdmin
 
 
 class ProgramAdmin(TranslatableAdmin):
     search_fields = ('translations__name',)
 
+
+class UrgentNeedsAdmin(TranslatableAdmin):
+    search_fields = ('translations__name',)
+    fields = ('name', 'description', 'link', 'type',
+              'phone_number', 'type_short', 'active')
+
+
 class NavigatorAdmin(TranslatableAdmin):
     search_fields = ('translations__name',)
 
+
 admin.site.register(Program, ProgramAdmin)
+admin.site.register(UrgentNeed, UrgentNeedsAdmin)
 admin.site.register(Navigator, NavigatorAdmin)
