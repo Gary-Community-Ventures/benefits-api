62ad952d4533d8f1b723512abb1cd4dbab6a9f81
Caleb <caleblpena@gmail.com>
2023-02-17 13:47:22 -0700

    added translations to text message

--- screener/communications/cell.py
+++ screener/communications/cell.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext as _
 from decouple import config
 from twilio.rest import Client
 
@@ -9,10 +10,12 @@ def text_link(cell, screen, language):
     auth_token = config("TWILIO_TOKEN")
     from_phone_number = config("TWILIO_PHONE_NUMBER")
     client = Client(account_sid, auth_token)
-
+    domain = config("FRONTEND_DOMAIN")
+    words = _("Here is a link to your results")
+    url = f"{domain}/results/{screen.uuid}"
     message = client.messages.create(
         from_=from_phone_number,
-        body=f"Here is a link to your results: https://screener.myfriendben.org/results/{screen.uuid}",
+        body=f"{words}: {url}",
         to="+1"+str(cell),
     )
 
