6662794be8f8878522ceda7617e34fcda192e336
Caleb <caleblpena@gmail.com>
2023-02-22 14:50:35 -0700

    added gettext to messages and added messages to acp

--- programs/programs/messages.py
+++ programs/programs/messages.py
@@ -1,11 +1,14 @@
+from django.utils.translation import gettext_noop as _
+
+
 def income(income, max_income):
     '''
     Household makes ${income} per month which must be less than ${max_income}
     '''
     return (
-        'Household makes',
+        _('Household makes'),
         f' ${round(income)} ',
-        'per month which must be less than',
+        _('per month which must be less than'),
         f' ${round(max_income)}'
     )
 
@@ -15,9 +18,9 @@ def child(min_age=0, max_age=18):
     Must have a child between the ages of {min_age} and {max_age}
     '''
     return (
-        'Must have a child between the ages of',
+        _('Must have a child between the ages of'),
         f' {min_age} ',
-        'and',
+        _('and'),
         f' {max_age}'
     )
 
@@ -27,9 +30,9 @@ def adult(min_age, max_age):
     Someone in the household must be between the ages of {min_age} and {max_age}
     '''
     return (
-        'Someone in the household must be between the ages of',
+        _('Someone in the household must be between the ages of'),
         f' {min_age} ',
-        'and',
+        _('and'),
         f' {max_age}'
     )
 
@@ -39,7 +42,7 @@ def must_have_benefit(benefit_name):
     Household must have {benefit_name}
     '''
     return (
-        'Household must have',
+        _('Household must have'),
         f' {benefit_name}'
     )
 
@@ -49,7 +52,7 @@ def must_not_have_benefit(benefit_name):
     Household must not have {benefit_name}
     '''
     return (
-        'Household must not have',
+        _('Household must have'),
         f' {benefit_name}'
     )
 
@@ -59,7 +62,7 @@ def location():
     Must live in an eligible location
     '''
     return (
-        'Must live in an eligible location',
+        _('Must live in an eligible location'),
     )
 
 
@@ -68,7 +71,7 @@ def has_disability():
     Someone in the household must have a disability
     '''
     return (
-        'Someone in the household must have a disability'
+        _('Someone in the household must have a disability')
     )
 
 
@@ -77,5 +80,5 @@ def has_no_insturance():
     Someone in the household must not have health insurance
     '''
     return (
-        'Someone in the household must not have health insurance'
+        _('Someone in the household must not have health insurance')
     )
