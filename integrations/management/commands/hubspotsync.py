4175cf0b7cfc9de33ed928b86e38beea44e2e7fb
Brian Hiatt <brian.hiatt@gmail.com>
2023-02-28 12:46:22 -0700

    Addressed multiple points of feedback from PR#56 and added the hubspot-api-client to requirements.txt

--- integrations/management/commands/hubspotsync.py
+++ integrations/management/commands/hubspotsync.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         limit = options['limit']
-        status = sync_mfb_hubspot_users(limit)
+        status = self.sync_mfb_hubspot_users(limit)
         if len(status['completed']) > 0:
             self.stdout.write(
                 self.style.SUCCESS('Successfully synced %s users.' % len(status['completed'])))
@@ -27,50 +27,50 @@ class Command(BaseCommand):
                 self.stdout.write(
                     self.style.SUCCESS('Successfully synced user %s with contact %s.' % (message[0], message[1])))
 
+    # MFB allows users to sign up to receive benefits updates or offers. If they do
+    # so PII is temporarily stored against their user record. This cron function
+    # queries the database every 10 minutes for new signups, syncs that data to
+    # hubspot, and then clears the PII on the user record in favor of storing the
+    # external id. This separates PII from household demographic.
+    def sync_mfb_hubspot_users(self, limit):
+        if limit > 10:
+            limit = 10  # prevent hitting the rate limit on the hubspot API
+        status = {
+            'processed': 0,
+            'completed': [],
+            'failed': []
+        }
+        screen = None
+        processed = 0
 
-# MFB allows users to sign up to receive benefits updates or offers. If they do
-# so PII is temporarily stored against their user record. This cron function
-# queries the database every 10 minutes for new signups, syncs that data to
-# hubspot, and then clears the PII on the user record in favor of storing the
-# external id. This separates PII from household demographic.
-def sync_mfb_hubspot_users(limit):
-    if limit > 10:
-        limit = 10  # prevent hitting the rate limit on the hubspot API
-    status = {
-        'processed': 0,
-        'completed': [],
-        'failed': []
-    }
-    processed = 0
+        # for now, we only sync users to hubspot who have signed up for offers or
+        # updates and given TCPA consent
+        unsynced = User.objects.filter(Q(send_offers=True) | Q(send_updates=True))\
+            .filter(external_id__isnull=True)\
+            .filter(tcpa_consent=True)
 
-    # for now, we only sync users to hubspot who have signed up for a
-    # communications and given TCPA consent
-    unsynced = User.objects.filter(Q(send_offers=True) | Q(send_updates=True))\
-        .filter(external_id__isnull=True)\
-        .filter(tcpa_consent=True)
-
-    for user in unsynced:
-        if processed < limit:
-            user_screens = Screen.objects.filter(user_id=user.id).order_by('-submission_date')
-            if user_screens:
-                screen = user_screens.first()
-            hubspot_id = upsert_user_hubspot(user, screen)
-            if hubspot_id:
-                replace_pii_with_hubspot_id(hubspot_id, user)
-                status['completed'].append((user.id, hubspot_id))
-            else:
-                status['failed'].append((user.id, hubspot_id))
-        processed += 1
-    return status
+        for user in unsynced:
+            if processed < limit:
+                user_screens = Screen.objects.filter(user_id=user.id).order_by('-submission_date')
+                if user_screens:
+                    screen = user_screens.first()
+                hubspot_id = upsert_user_hubspot(user, screen)
+                if hubspot_id:
+                    self.replace_pii_with_hubspot_id(hubspot_id, user)
+                    status['completed'].append((user.id, hubspot_id))
+                else:
+                    status['failed'].append((user.id, hubspot_id))
+            processed += 1
+        return status
 
 
-# stores an external id from hubspot and then clears all of the PII
-def replace_pii_with_hubspot_id(hubspot_id, user):
-    user.external_id = hubspot_id
-    user.email_or_cell = hubspot_id + "@myfriendben.org"
-    user.first_name = None
-    user.last_name = None
-    user.cell = None
-    user.email = None
-    user.save()
-    return user
+    # stores an external id from hubspot and then clears all of the PII
+    def replace_pii_with_hubspot_id(self, hubspot_id, user):
+        user.external_id = hubspot_id
+        user.email_or_cell = hubspot_id + "@myfriendben.org"
+        user.first_name = None
+        user.last_name = None
+        user.cell = None
+        user.email = None
+        user.save()
+        return user
