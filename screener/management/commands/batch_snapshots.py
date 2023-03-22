ced3994e6eeaafca9066110c464f88024cad1675
Caleb <caleblpena@gmail.com>
2023-03-22 12:25:57 -0600

    get correct screens for batch snapshot

---
+++ screener/management/commands/batch_snapshots.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from screener.models import Screen
+from programs.programs import calculators
+
+
+class Command(BaseCommand):
+    help = '''
+    Creates snapshots for all users.
+    Limit default is 1.
+    Defaults to only creating snapshots for users with emails.
+    '''
+
+    def add_arguments(self, parser):
+        parser.add_argument('--limit', default=1, type=int)
+        parser.add_argument('--all', default=False, type=bool)
+
+    def handle(self, *args, **options):
+        # Get the screens
+        screens = Screen.objects.filter(
+            agree_to_tos=True,
+            is_test=False,
+        )
+
+        if not options['all']:
+            screens = screens.exclude(user__isnull=True)
+
+        limit = None if options['limit'] == -1 else options['limit']
+        screens = screens.order_by('-submission_date')[:limit]
+
+        print(screens)
+        
+        # Calculate eligibility for each screen
+
+        # Add snapshots
