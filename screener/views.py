3cf42726a2bab61ca526f875137764aa2be45ae2
Caleb <caleblpena@gmail.com>
2023-09-21 16:04:33 -0600

    allow urgent needs to have multiple categories

--- screener/views.py
+++ screener/views.py
@@ -314,7 +314,7 @@ def urgent_need_results(screen):
         if has_need:
             list_of_needs.append(need)
 
-    urgent_need_resources = UrgentNeed.objects.filter(type_short__in=list_of_needs, active=True)
+    urgent_need_resources = UrgentNeed.objects.filter(type_short__name__in=list_of_needs, active=True).distinct()
 
     eligible_urgent_needs = []
     for need in urgent_need_resources:
