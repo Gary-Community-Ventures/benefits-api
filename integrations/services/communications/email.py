b600396264fcf3ce1c2cdb4505fbe1050872e287
Caleb <caleblpena@gmail.com>
2023-10-24 10:09:41 -0600

    translate emails

--- integrations/services/communications/email.py
+++ integrations/services/communications/email.py
@@ -3,9 +3,10 @@ from decouple import config
 import sendgrid
 from sendgrid.helpers.mail import Mail, Email, To, Content
 from screener.models import Screen
+from translations.models import Translation
 
 
-def email_pdf(target_email, screen_id, language):
+def email_link(target_email, screen_id, language):
     screen = Screen.objects.get(pk=screen_id)
 
     sg = sendgrid.SendGridAPIClient(api_key=config('SENDGRID'))
@@ -13,9 +14,10 @@ def email_pdf(target_email, screen_id, language):
     to_email = To(target_email)  # Change to your recipient
     domain = config("FRONTEND_DOMAIN")
     url = f"{domain}/{screen.uuid}/results"
-    subject = _("Screener Results from My Friend Ben")
+    words = Translation.objects.get(label='sendResults.email').get_lang(language).text
+    subject = Translation.objects.get(label='sendResults.email-subject').get_lang(language).text
     content = Content("text/html",
-                      _("Thank you for using MyFriendBen. Click here to review your results.") +
+                      words +
                       f' <a href="{url}">{url}</a>')
     mail = Mail(from_email, to_email, subject, content)
 
--- screener/communications/__init__.py
+++
@@ -1,2 +0,0 @@
-from screener.communications.email import email_pdf
-from screener.communications.cell import text_link
