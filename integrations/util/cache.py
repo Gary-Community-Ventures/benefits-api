84f82ef67849814d688c957f03f23b50c3fd88e4
Caleb <caleblpena@gmail.com>
2023-12-20 15:12:20 -0700

    add cache for leap and cccap

---
+++ integrations/util/cache.py
@@ -0,0 +1,30 @@
+from sentry_sdk import capture_message
+import datetime
+
+
+class Cache():
+    expire_time = 0
+
+    def __init__(self):
+        self.data = self.update()
+        self.last_update = datetime.datetime.now()
+
+    def update(self):
+        raise NotImplementedError()
+
+    def _update_cache(self):
+        try:
+            self.data = self.update()
+            self.last_update = datetime.datetime.now()
+        except Exception:
+            print(f'Failed to update {self.__class__.__name__}')
+            capture_message(f'Failed to update {self.__class__.__name__}', level='warning')
+
+    def should_update(self):
+        return datetime.datetime.now() > self.last_update + datetime.timedelta(seconds=self.expire_time)
+
+    def fetch(self):
+        if self.should_update():
+            self._update_cache()
+
+        return self.data
