aec01042b9c45448db2ef8c1d51805185170ab5f
Caleb <caleblpena@gmail.com>
2024-01-08 14:52:20 -0700

    refactor my spark

--- programs/programs/my_spark/calculator.py
+++ programs/programs/my_spark/calculator.py
@@ -1,77 +1,40 @@
+from programs.programs.calc import ProgramCalculator, Eligibility
 import programs.programs.messages as messages
 
 
-def calculate_my_spark(screen, data, program):
-    my_spark = MySpark(screen, data)
-    eligibility = my_spark.eligibility
-    value = my_spark.value
-
-    calculation = {
-        'eligibility': eligibility,
-        'value': value
-    }
-
-    return calculation
-
-
-class MySpark():
+class MySpark(ProgramCalculator):
     amount_per_child = 1_000
     max_age = 14
     min_age = 11
+    county = 'Denver County'
+    dependencies = ['age', 'county']
 
-    def __init__(self, screen, data):
-        self.screen = screen
-        self.data = data
+    def eligible(self) -> Eligibility:
+        e = Eligibility()
 
-        self.eligibility = {
-            "eligible": True,
-            "passed": [],
-            "failed": []
-        }
-
-        self.calc_eligibility()
-
-        self.calc_value()
-
-    def calc_eligibility(self):
         # Qualify for FRL
         is_frl_eligible = False
         for benefit in self.data:
             if benefit["name_abbreviated"] == 'nslp':
                 is_frl_eligible = benefit["eligible"]
                 break
-        self._condition(is_frl_eligible, messages.must_have_benefit('Free or Reduced Lunch'))
+        e.condition(is_frl_eligible, messages.must_have_benefit('Free or Reduced Lunch'))
 
         # Denever County
-        self._condition(self.screen.county == 'Denver County', messages.location())
+        e.condition(self.screen.county == MySpark.county, messages.location())
 
         # Kid 11 - 14
-        in_age_range = False
-        for member in self.screen.household_members.all():
-            if self._between(member.age, MySpark.min_age, MySpark.max_age):
-                in_age_range = True
-        self._condition(in_age_range, messages.child(MySpark.min_age, MySpark.max_age))
+        e.member_eligibility(
+            self.screen.household_members.all(),
+            [
+                (
+                    lambda m: m.age > MySpark.min_age and m.age < MySpark.max_age,
+                    messages.child(MySpark.min_age, MySpark.max_age)
+                )
+            ]
+        )
 
-    def calc_value(self):
-        value = 0
-        for member in self.screen.household_members.all():
-            if self._between(member.age, MySpark.min_age, MySpark.max_age):
-                value += MySpark.amount_per_child
+        return e
 
-        self.value = value
-
-    def _failed(self, msg):
-        self.eligibility["eligible"] = False
-        self.eligibility["failed"].append(msg)
-
-    def _passed(self, msg):
-        self.eligibility["passed"].append(msg)
-
-    def _condition(self, condition, msg):
-        if condition is True:
-            self._passed(msg)
-        else:
-            self._failed(msg)
-
-    def _between(self, value, min_val, max_val):
-        return min_val <= value <= max_val
+    def value(self, eligible_members: int):
+        return MySpark.amount_per_child * eligible_members
