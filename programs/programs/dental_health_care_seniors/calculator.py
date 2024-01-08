ad975500ae6f46493a6ba81d762e465f3dc7309b
Caleb <caleblpena@gmail.com>
2024-01-08 12:13:14 -0700

    refactor dental health care for seniors

--- programs/programs/dental_health_care_seniors/calculator.py
+++ programs/programs/dental_health_care_seniors/calculator.py
@@ -1,39 +1,16 @@
+from programs.programs.calc import ProgramCalculator, Eligibility
 import programs.programs.messages as messages
 
 
-def calculate_dental_health_care_seniors(screen, data, program):
-    cdhcs = DentalHealthCareSeniors(screen, program)
-    eligibility = cdhcs.eligibility
-    value = cdhcs.value
-
-    calculation = {
-        'eligibility': eligibility,
-        'value': value
-    }
-
-    return calculation
-
-
-class DentalHealthCareSeniors():
+class DentalHealthCareSeniors(ProgramCalculator):
     amount = 80
     min_age = 60
+    percent_of_fpl = 2.5
+    dependencies = ['age', 'income_amount', 'income_frequency', 'insurance']
 
-    def __init__(self, screen, program):
-        self.screen = screen
-        self.fpl = program.fpl.as_dict()
-
-        self.eligibility = {
-            "eligible": True,
-            "passed": [],
-            "failed": []
-        }
-
-        self.calc_eligibility()
-
-        self.calc_value()
-
-    def calc_eligibility(self):
-        self._member_eligibility(
+    def eligible(self) -> Eligibility:
+        e = Eligibility()
+        e.member_eligibility(
             self.screen.household_members.all(),
             [
                 (
@@ -48,37 +25,13 @@ class DentalHealthCareSeniors():
         )
 
         # Income test
+        fpl = self.program.fpl.as_dict()
         gross_income = int(self.screen.calc_gross_income("monthly", ["all"]))
-        income_band = int(2.5 * self.fpl[self.screen.household_size]/12)
-        self._condition(gross_income <= income_band,
-                        messages.income(gross_income, income_band))
+        income_band = int(DentalHealthCareSeniors.percent_of_fpl * fpl[self.screen.household_size] / 12)
+        e.condition(gross_income <= income_band,
+                    messages.income(gross_income, income_band))
 
-    def calc_value(self):
-        self.value = DentalHealthCareSeniors.amount * self.screen.num_adults(age_max=DentalHealthCareSeniors.min_age) * 12
+        return e
 
-    def _failed(self, msg):
-        self.eligibility["eligible"] = False
-        self.eligibility["failed"].append(msg)
-
-    def _passed(self, msg):
-        self.eligibility["passed"].append(msg)
-
-    def _condition(self, condition, msg):
-        if condition is True:
-            self._passed(msg)
-        else:
-            self._failed(msg)
-
-    def _member_eligibility(self, members, conditions):
-        '''
-        Filter out members that do not meet the condition and make eligibility messages
-        '''
-        if len(conditions) <= 0:
-            return members
-
-        [condition, message] = conditions.pop()
-        eligible_members = list(filter(condition, members))
-        if message:
-            self._condition(len(eligible_members) >= 1, message)
-
-        return self._member_eligibility(eligible_members, conditions)
+    def value(self):
+        return DentalHealthCareSeniors.amount * self.screen.num_adults(age_max=DentalHealthCareSeniors.min_age) * 12
