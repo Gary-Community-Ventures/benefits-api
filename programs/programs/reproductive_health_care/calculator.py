77da4b2bd029838a30f28d75284fbe3831f8be69
Caleb <caleblpena@gmail.com>
2024-01-08 15:37:00 -0700

    refactor rhc

--- programs/programs/reproductive_health_care/calculator.py
+++ programs/programs/reproductive_health_care/calculator.py
@@ -1,41 +1,17 @@
+from programs.programs.calc import ProgramCalculator, Eligibility
 import programs.programs.messages as messages
 
 
-def calculate_reproductive_health_care(screen, data, program):
-    rhc = ReproductiveHealthCare(screen, data)
-    eligibility = rhc.eligibility
-    value = rhc.value
-
-    calculation = {
-        'eligibility': eligibility,
-        'value': value
-    }
-
-    return calculation
-
-
-class ReproductiveHealthCare():
+class ReproductiveHealthCare(ProgramCalculator):
     amount = 268
+    dependencies = ['insurance']
 
-    def __init__(self, screen, data):
-        self.screen = screen
-        self.data = data
+    def eligible(self) -> Eligibility:
+        e = Eligibility()
 
-        self.eligibility = {
-            "eligible": True,
-            "passed": [],
-            "failed": []
-        }
-
-        self.calc_eligibility()
-
-        self.calc_value()
-
-    def calc_eligibility(self):
         # No health insurance
         has_no_hi = self.screen.has_insurance_types(('none',))
-        self._condition(has_no_hi,
-                        messages.has_no_insurance())
+        e.condition(has_no_hi, messages.has_no_insurance())
 
         # Medicade eligibility
         is_medicaid_eligible = False
@@ -44,21 +20,9 @@ class ReproductiveHealthCare():
                 is_medicaid_eligible = benefit["eligible"]
                 break
 
-        self._condition(is_medicaid_eligible,
-                        messages.must_have_benefit("Medicaid"))
+        e.condition(is_medicaid_eligible, messages.must_have_benefit("Medicaid"))
 
-    def calc_value(self):
-        self.value = ReproductiveHealthCare.amount
+        return e
 
-    def _failed(self, msg):
-        self.eligibility["eligible"] = False
-        self.eligibility["failed"].append(msg)
-
-    def _passed(self, msg):
-        self.eligibility["passed"].append(msg)
-
-    def _condition(self, condition, msg):
-        if condition is True:
-            self._passed(msg)
-        else:
-            self._failed(msg)
+    def value(self, eligible_members: int):
+        return ReproductiveHealthCare.amount
