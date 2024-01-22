02cfc445d01dece8b7f473ead6ecc0f4e3bfe6a4
Caleb <caleblpena@gmail.com>
2024-01-22 14:47:09 -0700

    remove leap condition and update presumptive eligibility for ubp

--- programs/programs/utility_bill_pay/calculator.py
+++ programs/programs/utility_bill_pay/calculator.py
@@ -20,27 +20,16 @@ class UtilityBillPay(ProgramCalculator):
     def eligible(self) -> Eligibility:
         e = Eligibility()
 
-        # is not eligible for LEAP
-        is_leap_eligible = False
-        for benefit in self.data:
-            if benefit["name_abbreviated"] == 'leap':
-                is_leap_eligible = benefit["eligible"]
-                break
-
-        e.condition(not is_leap_eligible, messages.must_not_have_benefit('LEAP'))
-
         # has other programs
         presumptive_eligible = False
         for benefit in UtilityBillPay.presumptive_eligibility:
             if self.screen.has_benefit(benefit):
                 presumptive_eligible = True
 
-        e.condition(presumptive_eligible)
-
         # income
         income = int(self.screen.calc_gross_income('yearly', ['all']))
         income_limit = UtilityBillPay.income_limits[self.screen.household_size - 1]
 
-        e.condition(income < income_limit, messages.income(income, income_limit))
+        e.condition(income < income_limit or presumptive_eligible, messages.income(income, income_limit))
 
         return e
