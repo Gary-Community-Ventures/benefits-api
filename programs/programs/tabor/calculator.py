8a77b782c4cbed6bb5035b80d2b6d95887981853
Caleb <caleblpena@gmail.com>
2024-01-23 11:52:29 -0700

    add tabor dependencies

--- programs/programs/tabor/calculator.py
+++ programs/programs/tabor/calculator.py
@@ -5,6 +5,7 @@ import programs.programs.messages as messages
 class Tabor(ProgramCalculator):
     min_age = 18
     amount = 800
+    dependencies = ['age']
 
     def eligible(self) -> Eligibility:
         e = Eligibility()
