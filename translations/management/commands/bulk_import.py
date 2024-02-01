0d9133610c6b12493b7a6dd3d2c62adaeaad0614
Caleb <caleblpena@gmail.com>
2024-02-01 14:49:43 -0700

    update bulk import to use stdin

--- translations/management/commands/bulk_import.py
+++ translations/management/commands/bulk_import.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from translations.bulk_import_translations import bulk_add
+from sys import stdin
 import argparse
 import json
 
@@ -10,7 +11,7 @@ class Command(BaseCommand):
     '''
 
     def add_arguments(self, parser):
-        parser.add_argument('data', type=argparse.FileType('r', encoding='utf8'))
+        parser.add_argument('data', nargs='?', type=argparse.FileType('r', encoding='utf8'), default=stdin)
 
     def handle(self, *args, **options):
         data = json.load(options['data'])
