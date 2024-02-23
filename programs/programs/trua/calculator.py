9c87e48b8904d2e69e70527a93d9166a7bee7ec5
Caleb <caleblpena@gmail.com>
2024-02-23 11:08:34 -0700

    update trua value

--- programs/programs/trua/calculator.py
+++ programs/programs/trua/calculator.py
@@ -2,6 +2,7 @@ from programs.programs.calc import Eligibility, ProgramCalculator
 import programs.programs.messages as messages
 from programs.co_county_zips import counties_from_zip
 
+
 class Trua(ProgramCalculator):
     income_limit = {
         1: 66_300,
@@ -15,7 +16,7 @@ class Trua(ProgramCalculator):
     }
 
     county = 'Denver County'
-    amount = 5568 + 382.83 + 235.68
+    amount = 6_500
     dependencies = ['income_amount', 'income_frequency', 'household_size', 'zipcode']
 
     def eligible(self) -> Eligibility:
@@ -24,7 +25,7 @@ class Trua(ProgramCalculator):
         # Income test
         gross_income = int(self.screen.calc_gross_income("monthly", ["all"]))
         income_limit = int(Trua.income_limit[self.screen.household_size]/12)
-        
+
         # Location test
         zipcode = self.screen.zipcode
         location = self.screen.county
@@ -36,8 +37,7 @@ class Trua(ProgramCalculator):
 
         # Denever County
         e.condition(Trua.county in counties, messages.location())
-        
-        e.condition(gross_income <= income_limit,
-                        messages.income(gross_income, income_limit))
-        
+
+        e.condition(gross_income <= income_limit, messages.income(gross_income, income_limit))
+
         return e
