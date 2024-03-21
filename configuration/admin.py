40fffbd7bf320ddd17922654858d83d96a450072
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2024-03-21 16:09:58 -0400

    New `StateSpecificModifier` model
    - Added a new model called `StateSpecificModifier` as part of the Configuration app's models
    - The main purpose of this model is to save hardcoded state specific value and modifiers

--- configuration/admin.py
+++ configuration/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Configuration
+from .models import Configuration, StateSpecificModifier
 
 class ConfigurationAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 admin.site.register(Configuration, ConfigurationAdmin)
+admin.site.register(StateSpecificModifier, ConfigurationAdmin)
--- configuration/models.py
+++ configuration/models.py
@@ -7,3 +7,12 @@ class Configuration(models.Model):
     
     def __str__(self):
         return self.name
+    
+
+class StateSpecificModifier(models.Model):
+    state = models.CharField(max_length=2)
+    name = models.CharField(max_length=320)
+    data = models.JSONField(default=dict)
+    
+    def __str__(self):
+        return self.name
