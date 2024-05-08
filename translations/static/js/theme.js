9bbf958a8ae501cc6480a67b5c2a386f9ac13a31
Marianna Demenko <65931890+Mdemenko1@users.noreply.github.com>
2024-05-08 14:00:39 -0400

    adjustimg to the PR comments

--- translations/static/js/theme.js
+++ translations/static/js/theme.js
@@ -1,27 +1,27 @@
 (() => {
-  "use strict";
-  const prefersDarkMode = window.matchMedia(
-    "(prefers-color-scheme: dark)"
-  ).matches;
-  const defaultTheme = prefersDarkMode ? "dark" : "light";
-  const preferredTheme = localStorage.getItem("theme");
-  const toggleDarkMode = document.querySelector("#bd-theme");
-  const isDarkTheme = localStorage.getItem("theme") === "dark";
-
-  if (!preferredTheme) {
-    localStorage.setItem("theme", defaultTheme);
-  }
-
-  document.documentElement.setAttribute(
-    "data-theme",
-    preferredTheme || defaultTheme
-  );
-
-  toggleDarkMode.checked = isDarkTheme ? true : false;
-
-  toggleDarkMode.addEventListener("change", function () {
-    const newTheme = isDarkTheme ? "light" : "dark";
-    localStorage.setItem("theme", newTheme);
-    document.documentElement.setAttribute("data-theme", newTheme);
-  });
-})();
+    "use strict";
+    const prefersDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const defaultTheme = prefersDarkMode ? "dark" : "light";
+    const preferredTheme = localStorage.getItem("theme");
+    const toggleDarkMode = document.querySelector("#bd-theme");
+  
+    if (!preferredTheme) {
+      localStorage.setItem("theme", defaultTheme);
+    }
+  
+    document.documentElement.setAttribute(
+      "data-theme",
+      preferredTheme || defaultTheme
+    );
+  
+    toggleDarkMode.checked = preferredTheme === "dark" ? true : false;
+  
+    toggleDarkMode.addEventListener("change", function () {
+      const isDarkTheme = localStorage.getItem("theme") === "dark";
+      const newTheme = isDarkTheme ? "light" : "dark";
+      localStorage.setItem("theme", newTheme);
+      document.documentElement.setAttribute("data-theme", newTheme);
+    });
+  })();
