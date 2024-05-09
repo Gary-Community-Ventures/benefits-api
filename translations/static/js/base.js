e22901dcd168f328b5c79e45870a42a1794abd2b
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2024-05-09 12:19:17 -0400

    updated dropdown menu logic

--- translations/static/js/base.js
+++ translations/static/js/base.js
@@ -61,7 +61,62 @@ function initializeSidebarMenu() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+// For handling the dropdown menu in the tables
+function initializeDropdowns() {
+  var dropdowns = document.querySelectorAll("#" + tableId + " .dropdown");
+  dropdowns.forEach(function (dropdown, index) {
+    var dropdownButton = dropdown.querySelector(".button");
+    var dropdownSpan = dropdown.querySelector(".material-symbols-outlined");
+    var dropdownContent = dropdown.querySelector(".dropdown-content");
+
+    dropdownButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      dropdown.classList.toggle("is-active");
+      if (dropdown.classList.contains("is-active")) {
+        dropdownSpan.textContent = "expand_less";
+        checkDropdownPosition(dropdown, dropdownContent);
+      } else {
+        dropdownSpan.textContent = "expand_more";
+        dropdown.classList.remove("is-top");
+      }
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    document.addEventListener("click", function (event) {
+      var isClickInside = dropdown.contains(event.target);
+      if (!isClickInside) {
+        dropdown.classList.remove("is-active");
+        dropdown.classList.remove("is-top");
+        dropdownSpan.textContent = "expand_more";
+      }
+    });
+
+    // Close dropdowns when the page is scrolled
+    window.addEventListener("scroll", function () {
+      dropdown.classList.remove("is-active");
+      dropdown.classList.remove("is-top");
+      dropdownSpan.textContent = "expand_more";
+    });
+  });
+}
+
+function checkDropdownPosition(dropdown, dropdownContent) {
+  var dropdownRect = dropdown.getBoundingClientRect();
+  var dropdownContentRect = dropdownContent.getBoundingClientRect();
+  var viewportHeight = window.innerHeight;
+
+  if (dropdownRect.bottom + dropdownContentRect.height > viewportHeight) {
+    dropdown.classList.remove("is-top");
+  } else {
+    dropdown.classList.add("is-top");
+  }
+}
+
+function initializeAll() {
   initializeTableSorting();
   initializeSidebarMenu();
-});
+  initializeDropdowns();
+}
+
+document.addEventListener("DOMContentLoaded", initializeAll);
+document.body.addEventListener("htmx:afterSwap", initializeAll);
