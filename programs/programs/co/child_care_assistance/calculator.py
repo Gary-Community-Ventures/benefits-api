d602fca09706e47c0d819fc87f0e1edf93abffdf
Caleb <caleblpena@gmail.com>
2024-06-20 13:33:06 -0600

    full cccap limits from new sheet

--- programs/programs/co/child_care_assistance/calculator.py
+++ programs/programs/co/child_care_assistance/calculator.py
@@ -1,13 +1,20 @@
 from programs.programs.calc import ProgramCalculator, Eligibility
 from integrations.services.sheets import GoogleSheetsCache
 from programs.co_county_zips import counties_from_zip
-import re
 import programs.programs.messages as messages
 
 
-class CCCAPCache(GoogleSheetsCache):
-    sheet_id = "1WzobLnLoxGbN_JfTuw3jUCZV5N7IA_0uvwEkIoMt3Wk"
-    range_name = "Sheet1!A14:J78"
+class CccapFplCache(GoogleSheetsCache):
+    sheet_id = "1otQxo_hZu2pS1_1EBsPLVKP9HYFCdcYWZKNM24dbjvg"
+    range_name = "current FPL %!A2:B"
+    default = {}
+
+    def update(self):
+        data = super().update()
+
+        county_fpls = {r[0] + ' County': int(r[1]) for r in data}
+
+        return county_fpls
 
 
 class ChildCareAssistance(ProgramCalculator):
@@ -18,7 +25,7 @@ class ChildCareAssistance(ProgramCalculator):
     max_age_afterschool_disabled = 19
     asset_limit = 1_000_000
     dependencies = ["age", "income_amount", "income_frequency", "zipcode", "household_size"]
-    county_values = CCCAPCache()
+    fpl_limits = CccapFplCache()
 
     def eligible(self) -> Eligibility:
         e = Eligibility()
@@ -28,22 +35,22 @@ class ChildCareAssistance(ProgramCalculator):
 
         e.condition(cccap_children > 0, messages.child(max_age=ChildCareAssistance.max_age_afterschool))
 
+        cccap_county_limits = self.fpl_limits.fetch()
+
         # location
         counties = counties_from_zip(self.screen.zipcode)
-        county_name = counties[0] if len(counties) > 0 else self.screen.county
-        cccap_county_data = self._cccap_county_values(county_name)
-        e.condition(bool(cccap_county_data), messages.location())
+        county_name = self.screen.county if self.screen.county is not None else counties[0]
+        e.condition(county_name in cccap_county_limits, messages.location())
 
         # income
         frequency = "yearly"
         gross_income = self.screen.calc_gross_income(frequency, ["all"])
         deductions = self.screen.calc_expenses(frequency, ["childSupport"])
         net_income = gross_income - deductions
-        if cccap_county_data:
-            income_limit = cccap_county_data[self.screen.household_size] * 12
-            e.condition(net_income < income_limit, messages.income(net_income, income_limit))
-        else:
-            e.eligible = False
+        fpl_percent = cccap_county_limits[county_name] / 100
+        fpl = self.program.fpl.as_dict()
+        income_limit = fpl[self.screen.household_size] * fpl_percent
+        e.condition(net_income <= income_limit, messages.income(net_income, income_limit))
 
         # assets
         e.condition(
@@ -86,26 +93,3 @@ class ChildCareAssistance(ProgramCalculator):
                 children += 1
 
         return children
-
-    def _cccap_county_values(self, county_name):
-        match = False
-        sheet_values = self.county_values.fetch()
-
-        cccap_county_name = county_name.replace(" County", "")
-        non_decimal = re.compile(r"[^\d.]+")
-
-        for row in sheet_values:
-            if row[0] == cccap_county_name:
-                match = {
-                    1: -1,
-                    2: float(non_decimal.sub("", row[2])),
-                    3: float(non_decimal.sub("", row[3])),
-                    4: float(non_decimal.sub("", row[4])),
-                    5: float(non_decimal.sub("", row[5])),
-                    6: float(non_decimal.sub("", row[6])),
-                    7: float(non_decimal.sub("", row[7])),
-                    8: float(non_decimal.sub("", row[8])),
-                    9: float(non_decimal.sub("", row[9])),
-                }
-
-        return match
