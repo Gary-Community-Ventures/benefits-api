2ae36654f155253383d97cbb2043d954aa55a048
Caleb <caleblpena@gmail.com>
2024-06-21 09:50:01 -0600

    fix nfp config

--- configuration/management/commands/add_config.py
+++ configuration/management/commands/add_config.py
@@ -2549,11 +2549,11 @@ class Command(BaseCommand):
                 "dentallowincseniors": {
                     "_label": "healthCareBenefits.dentallowincseniors",
                     "_default_message": "Low-cost dental care for people 60 years of age or older (Colorado Dental Health Program for Low-Income Seniors)",
-                }
-            },
-            "nfp": {
-                "_label": "healthCareBenefits.nfp",
-                "_default_message": "Personalized support for first-time parents (Nurse-Family Partnership)",
+                },
+                "nfp": {
+                    "_label": "healthCareBenefits.nfp",
+                    "_default_message": "Personalized support for first-time parents (Nurse-Family Partnership)",
+                },
             },
             "category_name": {"_label": "healthCare", "_default_message": "Health Care"},
         },
