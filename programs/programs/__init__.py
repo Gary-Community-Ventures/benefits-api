8f492929f983e31356e387b34bb012b61dddfd56
Brian Espinosa <bespinosa1931@gmail.com>
2024-07-08 11:52:06 -0500

    add nc_calculators and copy of aca for nc

--- programs/programs/__init__.py
+++ programs/programs/__init__.py
@@ -1,5 +1,6 @@
 from .co import co_calculators
+from .nc import nc_calculators
 from .federal import federal_calculators
 from .calc import ProgramCalculator
 
-calculators: dict[str, type[ProgramCalculator]] = {**co_calculators, **federal_calculators}
+calculators: dict[str, type[ProgramCalculator]] = {**co_calculators, **nc_calculators, **federal_calculators}
