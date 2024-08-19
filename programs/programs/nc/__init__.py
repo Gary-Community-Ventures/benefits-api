00fbe973a39e47f5fc7d44c265f6923c9cfcbfee
Brian Espinosa <bespinosa1931@gmail.com>
2024-08-19 17:46:08 -0500

    removes extra character

--- programs/programs/nc/__init__.py
+++ programs/programs/nc/__init__.py
@@ -1,7 +1,7 @@
 from .nc_aca.calculator import ACASubsidiesNC
 from ..calc import ProgramCalculator
 
-nc_calculators: dict[str, type[ProgramCalculator]] = {\
+nc_calculators: dict[str, type[ProgramCalculator]] = {
     "nc_aca": ACASubsidiesNC,
     
 }
