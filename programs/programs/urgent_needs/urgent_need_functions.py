09555a7e43978995f1d64426d0523a176bb51d16
Ivonne Hernandez <ivonne702@gmail.com>
2024-08-20 13:52:01 -0600

    Add rent or mtg expense requirement back in to DenverEmergencyAssistance

--- programs/programs/urgent_needs/urgent_need_functions.py
+++ programs/programs/urgent_needs/urgent_need_functions.py
@@ -408,8 +408,9 @@ class DenverEmergencyAssistance(UrgentNeedFunction):
         income_eligible = (
             self.screen.calc_gross_income("yearly", ["all"]) < fpl[self.screen.household_size] * self.fpl_percent
         )
+        has_rent_or_mortgage = self.screen.has_expense(["rent", "mortgage"])
 
-        return county_eligible and income_eligible
+        return county_eligible and income_eligible and has_rent_or_mortgage
 
 
 class EarlyIntervention(ChildAgeFunction):
