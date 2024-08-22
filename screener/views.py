c6ddc656dc58910328e1757b5f66373495e483cb
Brian Espinosa <bespinosa1931@gmail.com>
2024-08-21 22:51:53 -0500

    replaces nc_medicaid with *STATE_MEDICAID_OPTIONS

--- screener/views.py
+++ screener/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from integrations.services.communications import MessageUser
+from programs.programs.helpers import STATE_MEDICAID_OPTIONS
 from programs.programs.policyengine.calculators import all_calculators
 from screener.models import (
     Screen,
@@ -202,8 +203,7 @@ def eligibility_results(screen, batch=False):
     pe_programs = pe_calculators.keys()
 
     def sort_first(program):
-        calc_first = ("tanf", "ssi", "medicaid", "nslp", "leap", "nc_medicaid")
-
+        calc_first = ("tanf", "ssi", "nslp", "leap", *STATE_MEDICAID_OPTIONS)
         if program.name_abbreviated in calc_first:
             print(program.name_abbreviated, "-0")
             return 0
