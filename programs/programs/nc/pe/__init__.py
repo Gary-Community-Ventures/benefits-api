49126d57aafb19f68639f0d6779da938e6bda610
Caleb <caleblpena@gmail.com>
2024-09-12 11:17:11 -0600

    update ssdi child eligibility

--- programs/programs/nc/pe/__init__.py
+++ programs/programs/nc/pe/__init__.py
@@ -2,10 +2,7 @@ import programs.programs.nc.pe.member as member
 from programs.programs.policyengine.calculators.base import PolicyEngineCalulator
 
 
-nc_member_calculators = {
-    "nc_medicaid": member.NcMedicaid,
-    "nc_wic": member.NcWic
-}
+nc_member_calculators = {"nc_medicaid": member.NcMedicaid, "nc_wic": member.NcWic}
 
 
 nc_pe_calculators: dict[str, type[PolicyEngineCalulator]] = {
