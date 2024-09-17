a7476c1818ec54018aef3d1efbbb28a7312c0911
Ivonne Hernandez <ivonne702@gmail.com>
2024-09-17 13:59:49 -0600

    Add a DontShow translation override calculator

---
+++ programs/programs/translation_overrides/dont_show.py
@@ -0,0 +1,9 @@
+from programs.programs.translation_overrides.base import TranslationOverrideCalculator
+
+
+class DontShow(TranslationOverrideCalculator):
+  def eligible(self) -> bool:
+    '''
+    Never use this override
+    '''
+    return False
