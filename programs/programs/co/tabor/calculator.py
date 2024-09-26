fb6793841b5123739f56bdbba36fa9ff4af59921
Caleb <caleblpena@gmail.com>
2024-09-26 15:23:52 -0600

    fix tabor eligibilty method

--- programs/programs/co/tabor/calculator.py
+++ programs/programs/co/tabor/calculator.py
@@ -1,5 +1,4 @@
-from programs.programs.calc import ProgramCalculator
-from screener.models import HouseholdMember
+from programs.programs.calc import MemberEligibility, ProgramCalculator
 
 
 class Tabor(ProgramCalculator):
@@ -7,6 +6,8 @@ class Tabor(ProgramCalculator):
     member_amount = 800
     dependencies = ["age"]
 
-    def member_eligible(self, member: HouseholdMember, e: Eligibility):
+    def member_eligible(self, e: MemberEligibility):
+        member = e.member
+
         # age
         e.condition(member.age >= Tabor.min_age)
