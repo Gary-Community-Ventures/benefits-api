c37fab4a4465f47ade743fa744fc19a7cd5a6c82
h0und <79583632+msrezaie@users.noreply.github.com>
2024-10-25 15:19:24 -0400

    used PE variables to get medicaid eligibility

--- programs/programs/federal/pe/member.py
+++ programs/programs/federal/pe/member.py
@@ -33,11 +33,14 @@ class Medicaid(PolicyEngineMembersCalculator):
     pe_inputs = [
         dependency.member.AgeDependency,
         dependency.member.PregnancyDependency,
+        dependency.member.SsiCountableResourcesDependency,
         *dependency.irs_gross_income,
     ]
     pe_outputs = [
         dependency.member.AgeDependency,
         dependency.member.Medicaid,
+        dependency.member.MedicaidCategory,
+        dependency.member.MedicaidSeniorOrDisabled,
     ]
 
     child_medicaid_average = 0
--- programs/programs/nc/pe/member.py
+++ programs/programs/nc/pe/member.py
@@ -1,3 +1,5 @@
+from programs.programs import messages
+from programs.programs.calc import Eligibility
 import programs.programs.policyengine.calculators.dependencies as dependency
 from programs.programs.federal.pe.member import Medicaid
 from programs.programs.federal.pe.member import Wic
@@ -5,83 +7,44 @@ from screener.models import HouseholdMember
 
 
 class NcMedicaid(Medicaid):
+    medicaid_categories = {
+        "NONE": 0,
+        "ADULT": 512,  # * 12 = 6146,  Medicaid Expansion Adults
+        "INFANT": 372,  # * 12 = 4464,  Medicaid for Children
+        "YOUNG_CHILD": 372,  # * 12 = 4464,  Medicaid for Children
+        "OLDER_CHILD": 372,  # * 12 = 4464,  Medicaid for Children
+        "PREGNANT": 1044,  # * 12 = 12536, Medicaid for Pregnant Women
+        "YOUNG_ADULT": 512,  # * 12 = 6146,  Medicaid Expansion Adults
+        "PARENT": 0,
+        "SSI_RECIPIENT": 0,
+        "AGED": 1518,  # * 12 = 13035, Medicaid for the Aged
+        "DISABLED": 1086,  # * 12 = 18227, Medicaid for the Disabled
+    }
+
     pe_inputs = [
         *Medicaid.pe_inputs,
         dependency.household.NcStateCode,
+        dependency.member.IsDisabledDependency,
     ]
 
-    subcategory_values = {
-        'MAD': 18227,  # Medicaid for the Disabled
-        'MAA': 13035,  # Medicaid for the Aged
-        'MPW': 12536,  # Medicaid for Pregnant Women
-        'MXP': 6146,   # Medicaid Expansion Adults
-        'MIC': 4464,   # Medicaid for Children
-    }
-
-    subcategory_fpl_percentages = {
-        'MIC': 211,
-        'MPW': 196,
-        'MXP': 133,
-        'MAD': 100,
-        'MAA': 100,
-    }
-
-    def determine_subcategory(self, member: HouseholdMember):
-        if self.is_eligible_mad(member):
-            return 'MAD'
-        elif self.is_eligible_maa(member):
-            return 'MAA'
-        elif self.is_eligible_mpw(member):
-            return 'MPW'
-        elif self.is_eligible_mxp(member):
-            return 'MXP'
-        elif self.is_eligible_mic(member):
-            return 'MIC'
-        else:
-            return None 
-
-    def is_eligible_mad(self, member: HouseholdMember):
-        return member.has_disability() and self.is_income_within_limit(member, 'MAD')
-
-    def is_eligible_maa(self, member: HouseholdMember):
-        return member.age >= 65 and self.is_income_within_limit(member, 'MAA')
-
-    def is_eligible_mpw(self, member: HouseholdMember):
-        return member.pregnant and self.is_income_within_limit(member, 'MPW')
-
-    def is_eligible_mxp(self, member: HouseholdMember):
-        return (
-            19 <= member.age <= 64
-            and not member.pregnant
-            and not member.has_disability()
-            and self.is_income_within_limit(member, 'MXP')
-        )
-
-    def is_eligible_mic(self, member: HouseholdMember):
-        return member.age <= 18 and self.is_income_within_limit(member, 'MIC')
-
-    def is_income_within_limit(self, member: HouseholdMember, subcategory: str):
-        fpl_percentage = self.subcategory_fpl_percentages.get(subcategory, 0)
-        household_size = self.screen.household_size
-
-        if member.pregnant:
-            household_size += 1
-
-        fpl = self.program.fpl
-        income_limit = int(
-            fpl_percentage * fpl.get_limit(household_size) / 100
-        )
-
-        gross_income = int(self.screen.calc_gross_income("yearly", ["all"]))
-        return gross_income <= income_limit
-
     def member_value(self, member: HouseholdMember):
-        subcategory = self.determine_subcategory(member)
-        if subcategory:
-            estimated_value = self.subcategory_values[subcategory]
-            return estimated_value
-        else:
+        if self.get_member_variable(member.id) <= 0:
             return 0
+
+        medicaid_category = self.sim.value("people", str(member.id), "medicaid_category", self.pe_period)
+
+        # In Policy Engine, senior and disabled are not included in the medicaid categories variable.
+        # Instead, a separate variable is used to determine the medicaid eligiblity for a senior or disabled member.
+        is_senior_or_disabled = self.sim.value(
+            "people", str(member.id), "is_optional_senior_or_disabled_for_medicaid", self.pe_period
+        )
+
+        if is_senior_or_disabled:
+            if member.has_disability():
+                return self.medicaid_categories["DISABLED"] * 12
+            elif member.age >= 65:
+                return self.medicaid_categories["AGED"] * 12
+        return self.medicaid_categories[medicaid_category] * 12
 
 
 class NcWic(Wic):
