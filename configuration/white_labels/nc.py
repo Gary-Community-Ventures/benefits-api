fd2e8833d61c2de23e0b88c53195381f07830894
SonaliBedge <76449932+sona80@users.noreply.github.com>
2024-12-26 21:40:48 -0800

    update nc.py with black_formatter for Python files

--- configuration/white_labels/nc.py
+++ configuration/white_labels/nc.py
@@ -5,6 +5,7 @@ from screener.models import WhiteLabel
 # TODO: Update NC configuration
 class NcConfigurationData(ConfigurationData):
     @classmethod
+    
     def get_white_label(self) -> WhiteLabel:
         return WhiteLabel.objects.get(code="nc")
 
