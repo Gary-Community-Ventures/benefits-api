a3a4af60a8b7c834c8dd1bcd579f1fce3ab02f25
Caleb <caleblpena@gmail.com>
2025-01-08 10:46:51 -0700

    remove fpl API request because it is broken

--- programs/models.py
+++ programs/models.py
@@ -58,6 +58,7 @@ class FplCache(Cache):
         """
         Get FPLs for all relevant years using the official ASPE Poverty Guidelines API
         """
+        return self.default
         fpls = FederalPoveryLimit.objects.filter(fpl__isnull=False).distinct()
         fpl_dict = {}
         for fpl in fpls:
