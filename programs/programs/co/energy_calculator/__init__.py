4e019089d219e880e9fe77d652482a75346efd31
Caleb <caleblpena@gmail.com>
2025-01-30 12:48:14 -0700

    Refactor energy assistance expense check so the energy calculator can work differently

--- programs/programs/co/energy_calculator/__init__.py
+++ programs/programs/co/energy_calculator/__init__.py
@@ -1,9 +1,11 @@
 from programs.programs.calc import ProgramCalculator
 from programs.programs.co.energy_calculator.affordable_residential_energy.calculator import AffordableResidentialEnergy
+from programs.programs.co.energy_calculator.energy_assistance.calculator import EnergyCalculatorEnergyAssistance
 from programs.programs.co.energy_calculator.energy_outreach_solar.calculator import EnergyOutreachSolar
 
 
 co_energy_calculators: dict[str, type[ProgramCalculator]] = {
     "co_energy_calculator_care": AffordableResidentialEnergy,
     "co_energy_calculator_eocs": EnergyOutreachSolar,
+    "co_energy_calculator_leap": EnergyCalculatorEnergyAssistance,
 }
