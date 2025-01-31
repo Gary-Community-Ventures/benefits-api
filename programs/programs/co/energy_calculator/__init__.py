6010c51a757995e85f5317915f1f923f2b11ddbe
Caleb <caleblpena@gmail.com>
2025-01-31 14:33:29 -0700

    Add Emergency Assistance calculator

--- programs/programs/co/energy_calculator/__init__.py
+++ programs/programs/co/energy_calculator/__init__.py
@@ -1,6 +1,7 @@
 from programs.programs.calc import ProgramCalculator
 from programs.programs.co.energy_calculator.affordable_residential_energy.calculator import AffordableResidentialEnergy
 from programs.programs.co.energy_calculator.energy_assistance.calculator import EnergyCalculatorEnergyAssistance
+from programs.programs.co.energy_calculator.energy_outreach.calculator import EnergyCalculatorEmergencyAssistance
 from programs.programs.co.energy_calculator.energy_outreach_solar.calculator import EnergyOutreachSolar
 from programs.programs.co.energy_calculator.property_credit_rebate.calculator import (
     EnergyCalculatorPropertyCreditRebate,
@@ -14,4 +15,5 @@ co_energy_calculators: dict[str, type[ProgramCalculator]] = {
     "co_energy_calculator_leap": EnergyCalculatorEnergyAssistance,
     "co_energy_calculator_ubp": EnergyCalculatorUtilityBillPay,
     "co_energy_calculator_cpcr": EnergyCalculatorPropertyCreditRebate,
+    "co_energy_calculator_ea": EnergyCalculatorEmergencyAssistance,
 }
