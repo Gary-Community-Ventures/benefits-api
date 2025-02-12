b8719e2923cdc0969420e7e039a7221cc1529e56
IrynaKolh <iryna.kolhanova@gmail.com>
2025-02-12 12:34:47 -0500

    ubdate configs

--- configuration/white_labels/_default.py
+++ configuration/white_labels/_default.py
@@ -42,8 +42,8 @@ class DefaultConfigurationData(ConfigurationData):
         "featureFlags": {"default": []},
         "noResultMessage": {
             "default": {
-                "id": "noResultMessage",
-                "defaultMessage": "It looks like you may not qualify for benefits included in MyFriendBen at this time. If you indicated need for an immediate resource, please click on the “Near-Term Benefits” tab. For additional resources, please click the 'More Help' button below to get the resources you’re looking for.",
+                "_label": "noResultMessage",
+                "_default_message": "It looks like you may not qualify for benefits included in MyFriendBen at this time. If you indicated need for an immediate resource, please click on the “Near-Term Benefits” tab. For additional resources, please click the 'More Help' button below to get the resources you’re looking for.",
             },
         },
     }
--- configuration/white_labels/nc.py
+++ configuration/white_labels/nc.py
@@ -1740,12 +1740,8 @@ class NcConfigurationData(ConfigurationData):
         "featureFlags": {"default": []},
         "noResultMessage": {
             "default": {
-                "id": "noResultMessage",
-                "defaultMessage": "It looks like you may not qualify for benefits included in MyFriendBen at this time. If you indicated need for an immediate resource, please click on the “Near-Term Benefits” tab. For additional resources, please click the 'More Help' button below to get the resources you’re looking for.",
-            },
-            "ccig": {
-                "id": "ccig.results.message",
-                "defaultMessage": "Thank you for completing your application to join the Colorado Community Insight Group by filling out MyFriendBen. Your application will be processed within 2 - 4 days. In the meantime, we encourage you to explore the benefits and resources that may be available to your family listed below. If you have any questions in the meantime, please feel free to email us at ccig@garycommunity.org.",
+                "_label": "noResultMessage",
+                "_default_message": "It looks like you may not qualify for benefits included in MyFriendBen at this time. If you indicated need for an immediate resource, please click on the “Near-Term Benefits” tab. For additional resources, please click the 'More Help' button below to get the resources you’re looking for.",
             },
         },
     }
