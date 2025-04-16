c267154525c4e2112f29f8296d99265a821e5f28
Caleb <caleblpena@gmail.com>
2025-04-16 15:11:07 -0600

    energy ebt

---
+++ programs/programs/co/energy_calculator/energy_ebt/calculator.py
@@ -0,0 +1,17 @@
+from programs.programs.calc import Eligibility, ProgramCalculator
+
+
+class EnergyCalculatorEnergyEbt(ProgramCalculator):
+    amount = 21
+    max_fpl = 2
+    dependencies = ["income_frequency", "income_amount", "household_size"]
+
+    def household_eligible(self, e: Eligibility):
+        # income
+        income = self.screen.calc_gross_income("yearly", ["all"])
+        income_limit = self.program.year.as_dict()[self.screen.household_size] * self.max_fpl
+        e.condition(income <= income_limit)
+
+        # no LEAP
+        can_get_leap = self.screen.has_leap or self.data["co_energy_calculator_leap"]
+        e.condition(not can_get_leap)
