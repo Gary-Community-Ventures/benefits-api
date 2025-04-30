d791e86601a6ba148a6147116caabad243d362f3
Caleb <caleblpena@gmail.com>
2025-04-30 16:03:59 -0600

    remove white label from UrgentNeedCategory

--- programs/programs/urgent_needs/ma/heartwap.py
+++ programs/programs/urgent_needs/ma/heartwap.py
@@ -10,5 +10,15 @@ class Heartwap(UrgentNeedFunction):
         # income
         income_limit = smi.get_screen_smi(self.screen, self.urgent_need.year.period) * self.max_income_percent
         income = self.screen.calc_gross_income("yearly", ["all"])
+        income_eligible = income <= income_limit
 
-        return income <= income_limit
+        # expenses
+        has_expense = self.screen.has_expense(['rent', 'mortgage', 'subsidizedRent'])
+
+        print(f"income_limit: {income_limit}")
+        print(f"income: {income}")
+        print(f"income_eligible: {income_eligible}")
+        print(f"has_expense: {has_expense}")
+
+
+        return income_eligible and has_expense
