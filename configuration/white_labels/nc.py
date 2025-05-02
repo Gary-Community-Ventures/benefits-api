6a81f6ae845a3b10f19ce685de14f40a4d2603f8
IrynaKolh <iryna.kolhanova@gmail.com>
2025-05-02 16:43:13 -0400

    update nc.py config

--- configuration/white_labels/nc.py
+++ configuration/white_labels/nc.py
@@ -217,8 +217,9 @@ class NcConfigurationData(ConfigurationData):
         },
         "ncchwa": {
             "_label": "referralOptions.NCCHWA",
-            "_default_message": "North Carolina Community Health Worker Association (NCCHWA)",
+            "_default_message": "NC Community Health Worker Association (NCCHWA)",
         },
+        "ncnc": {"_label": "referralOptions.ncnc", "_default_message": "NC Navigators Consortium"},
         "onTrackFin": {
             "_label": "referralOptions.onTrackFin",
             "_default_message": "On Track Financial",
