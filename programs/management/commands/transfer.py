8e34ab8731069ef3ad0ea250033cf9a2489effdf
Sonali Bedge <sonali@codethedream.org>
2025-05-16 15:37:55 -0700

    made small change

--- programs/management/commands/transfer.py
+++ programs/management/commands/transfer.py
@@ -79,7 +79,7 @@ class Command(BaseCommand):
                         new_translation = Translation.objects.add_translation(
                             label=f"program.{source_program.external_name}_{source_program.id}-{field}-{str(uuid.uuid4())}",
                             default_message=source_translation.default_message,
-                            active=False,
+                            active=source_translation.active,
                             no_auto=source_translation.no_auto,
                         )
 
@@ -117,10 +117,14 @@ class Command(BaseCommand):
                 new_program.required_programs.set([])
                 new_program.documents.set([])
                 new_program.legal_status_required.set(legal_statuses)
-                self.stdout.write("Reminder: Please add external names to the transferred programs.")
+                self.stdout.write(
+                    "Reminder: Please add external names to the transferred programs."
+                )
 
             except Program.DoesNotExist:
-                self.stdout.write(self.style.ERROR(f"Error: Program '{external_name}' not found"))
+                self.stdout.write(
+                    self.style.ERROR(f"Error: Program '{external_name}' not found")
+                )
                 continue
             except Exception as e:
                 self.stdout.write(self.style.ERROR(f"Error during transfer: {str(e)}"))
