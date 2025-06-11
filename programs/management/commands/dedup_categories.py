796de31ad4ba332dd14cf68add8ae2673977feb4
msrezaie <79583632+msrezaie@users.noreply.github.com>
2025-06-11 10:38:21 -0400

    format fix

--- programs/management/commands/dedup_categories.py
+++ programs/management/commands/dedup_categories.py
@@ -11,11 +11,11 @@ class Command(BaseCommand):
         UrgentNeed = apps.get_model("programs", "UrgentNeed")
         UrgentNeedCategory = apps.get_model("programs", "UrgentNeedCategory")
 
-        all_categories = list(UrgentNeedCategory.objects.all().values('id', 'name'))
+        all_categories = list(UrgentNeedCategory.objects.all().values("id", "name"))
 
         name_to_ids = defaultdict(list)
         for category in all_categories:
-            name_to_ids[category['name']].append(category['id'])
+            name_to_ids[category["name"]].append(category["id"])
 
         id_mapping = {}
         for ids in name_to_ids.values():
@@ -23,12 +23,12 @@ class Command(BaseCommand):
             for cid in ids:
                 id_mapping[cid] = first_id
 
-        for urgent_need in UrgentNeed.objects.prefetch_related('type_short'):
-            current_categories = set(urgent_need.type_short.values_list('id', flat=True))
+        for urgent_need in UrgentNeed.objects.prefetch_related("type_short"):
+            current_categories = set(urgent_need.type_short.values_list("id", flat=True))
             non_dup_categories = {id_mapping[cid] for cid in current_categories}
             if current_categories != non_dup_categories:
                 urgent_need.type_short.set(non_dup_categories)
-        
+
         for name, ids in name_to_ids.items():
             first_id = min(ids)
             duplicates_to_delete = [cid for cid in ids if cid != first_id]
