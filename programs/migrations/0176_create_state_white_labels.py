67484611c8fc544dfa65afc15dbef6fa02c0736e
Elana Tee <elanatee@gmail.com>
2026-09-11 14:46:27 -0600

    MFB-1760: move migration file 0175 -> 0176 and update dependency

--- programs/migrations/0176_create_state_white_labels.py
+++ programs/migrations/0176_create_state_white_labels.py
@@ -84,7 +84,7 @@ def reverse_create(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("programs", "0174_consolidate_missed_program_categories"),
+        ("programs", "0175_ks_lieap_on_has_benefits_step"),
         ("screener", "0164_householdmember_was_in_foster_care"),
     ]
 
